d67d10e32174319a5cc4291cffc1a76b9ff6e94d
prasadrahane06 <prasadrahane06@gmail.com>
2024-03-05 22:42:05 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import React from "react";
+import UserContext from "./context/UserContext";
+import { UserContextProvider } from "./context/UserContextProvider";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
 
 function App() {
   return (
-    <>
-      <h1>Prasd</h1>
-    </>
+    <UserContextProvider>
+      <h1>Prasad</h1>
+      <Login />
+      <Profile />
+    </UserContextProvider>
   );
 }
 
