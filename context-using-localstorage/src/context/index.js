fa28744b3b7e2508f8f18aa41aad4cad0de00ba1
prasadrahane06 <prasadrahane06@gmail.com>
2024-03-07 00:11:24 +0530

    update context-theme-switcher/index.html

---
+++ context-using-localstorage/src/context/index.js
@@ -0,0 +1,6 @@
+//file made to take all import here and allow all the imports to be imported from the same file only
+export {
+  TodoContextProvider,
+  TodoContexts,
+  useTodoContext,
+} from "./TodoContexts";
