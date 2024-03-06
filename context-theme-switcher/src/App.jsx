85beab20e83fcbe5a79e144d04cb6da9aec53355
prasadrahane06 <prasadrahane06@gmail.com>
2024-03-06 07:55:06 +0530

    update App.jsx, Theme.js and tailwind.config.js

--- context-theme-switcher/src/App.jsx
+++ context-theme-switcher/src/App.jsx
@@ -1,14 +1,41 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import { ThemeContextProvider } from "./Context/Theme";
+import Themebtn from "./components/Themebtn";
+import Card from "./components/Card";
 
 function App() {
-  return (
-    <div className="flex flex-wrap min-h-screen items-center">
-      <div className="w-full">
-        <div className="w-full max-w-sm mx-auto flex justify-end mb-4"></div>
+  const [themeMode, setThemeMode] = useState("light");
 
-        <div className="w-full max-w-sm mx-auto"></div>
+  const lightTheme = () => {
+    setThemeMode("light");
+  };
+
+  const darkTheme = () => {
+    setThemeMode("dark");
+  };
+
+  // actual change in theme
+
+  useEffect(() => {
+    document.querySelector("html").classList.remove("light", "dark");
+    document.querySelector("html").classList.add(themeMode);
+  }, [themeMode]);
+
+  return (
+    <ThemeContextProvider value={{ themeMode, lightTheme, darkTheme }}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            <Themebtn />
+          </div>
+
+          <div className="w-full max-w-sm mx-auto">
+            <Card />
+          </div>
+        </div>
       </div>
-    </div>
+    </ThemeContextProvider>
   );
 }
 
--- context-theme-switcher/src/Context/Theme.js
+++ context-theme-switcher/src/Context/Theme.js
@@ -1,1 +1,11 @@
-im
+import { createContext, useContext } from "react";
+
+export const ThemeContext = createContext({
+  themeMode: "light",
+  darkThemeMode: () => {},
+  lightThemeMode: () => {},
+});
+
+export const ThemeContextProvider = ThemeContext.Provider;
+
+export const useTheme = () => useContext(ThemeContext);
