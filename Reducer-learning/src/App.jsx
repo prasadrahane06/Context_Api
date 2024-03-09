76c94408bff5e8e615b8d2e8410116bf68af6858
prasadrahane06 <prasadrahane06@gmail.com>
2024-03-09 20:25:13 +0530

    update context-theme-switcher/index.html and context-using-localstorage/index.html

---
+++ Reducer-learning/src/App.jsx
@@ -0,0 +1,38 @@
+import React, { useReducer, useState } from "react";
+import reducer from "./reducer";
+
+const initialState = {
+  count: 0,
+};
+
+function App() {
+  // const [count, setCount] = useState(0);
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const handleIncrementClick = () => {
+    dispatch({ type: "INCREMENT" });
+  };
+  const handleDecrementClick = () => {
+    dispatch({ type: "DECREMENT" });
+  };
+  return (
+    <div className="grid place-content-center items-center  w-full h-screen  ">
+      <div className="border-2 border-black w-60 h-40 flex flex-col items-center content-center:bg-yellow-200 sm:bg-slate-300">
+        <div className=" items-center place-content-center flex mt-3">
+          <p className="text-red-700 font-bold text-4xl">{state.count}</p>
+        </div>
+        <div className="w-[110px] bg-slate-600 p-1 mt-3 rounded-xl text-white flex place-content-center ">
+          <button className="font-bold text-xl" onClick={handleIncrementClick}>
+            Increment
+          </button>
+        </div>
+        <div className="w-[110px] bg-slate-600 p-1 mt-3 rounded-xl text-white">
+          <button className="font-bold text-xl" onClick={handleDecrementClick}>
+            Decrement
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
